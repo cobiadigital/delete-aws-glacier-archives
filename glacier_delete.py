0e1ac046e4c1e25b9e56b2c4be414838ee6deab8
cobiadigital <ben.brenner@gmail.com>
2021-03-11 14:42:20 -0600

    Update glacier_delete.py
    I get a "TypeError: unsupported operand type(s) for %: 'NoneType' and 'tuple'" error on the original. Perhaps a python vs issue?

--- glacier_delete.py
+++ glacier_delete.py
@@ -14,5 +14,5 @@ with open(json_file) as f:
 for item in data['ArchiveList']:
     cmd = "aws glacier delete-archive --account-id %s --vault-name %s --archive-id=\'%s\'" % (account_id, vault_name, item['ArchiveId'])
     returned_value = subprocess.call(cmd, shell=True)
-    print('cmd: %s || returned value: %s') % (cmd, str(returned_value))
+    print('cmd: %s || returned value: %s' % (cmd, str(returned_value)))
     time.sleep(0.1)
